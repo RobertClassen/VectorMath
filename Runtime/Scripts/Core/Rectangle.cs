25086405e31b95fb157c4b9df0955fa2053a50c8
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2021-10-17 22:30:00 +0200

    importd Core class

---
+++ Runtime/Scripts/Core/Rectangle.cs
@@ -0,0 +1,13 @@
+namespace WellDefinedValues.Core
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+
+	public static class Rectangle
+	{
+		public const float ExpansionFactor = 2f;
+		public const float IndentationWidth = 15f;
+		public const float SpaceDefault = 2f;
+	}
+}
