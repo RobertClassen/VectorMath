ec7de69fc91d10717474b37762d3782878d8ce60
Robert Classen <61211020+RobertClassen@users.noreply.github.com>
2022-01-10 19:32:14 +0100

    added Lerp for Rect

---
+++ Runtime/Scripts/Extensions/Rects/Float/RectExtensions.Lerp.cs
@@ -0,0 +1,22 @@
+﻿namespace VectorMath
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using NumericMath;
+	using NumericMath.Core;
+	using UnityEngine;
+
+	public static partial class RectExtensions
+	{
+		public static float LerpX(this Rect rect, float t, bool isClamped = Numeric.IsLerpClampedDefault)
+		{
+			return rect.xMin.Lerp(rect.xMax, t, isClamped);
+		}
+
+		public static float LerpY(this Rect rect, float t, bool isClamped = Numeric.IsLerpClampedDefault)
+		{
+			return rect.yMin.Lerp(rect.yMax, t, isClamped);
+		}
+	}
+}
